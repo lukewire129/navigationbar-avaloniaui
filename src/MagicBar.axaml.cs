1015d0ee96b1f444268f018fb7648c7f53c1bc37
lukewire129 <lukewire129@gmail.com>
2024-01-18 12:29:33 +0900

    Inidicator Move

--- src/MagicBar.axaml.cs
+++ src/MagicBar.axaml.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Controls.Templates;
+using Avalonia.Media;
 
 namespace AvaloniaNavigationBar;
 
@@ -17,5 +20,19 @@ public class MagicBar : ListBox
     }
     public MagicBar()
     {
+        this.SelectionChanged += (sender, args) =>
+        {
+            int idx = SelectedIndex;
+            Canvas.SetLeft(_circle, idx * 80);
+        };
+    }
+
+    private Grid _circle;
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+
+        _circle = e.NameScope.Get<Grid>("PART_Circle");
+        this.SelectedIndex = 0;
     }
 }
